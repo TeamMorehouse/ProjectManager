672d16df7703588f563b5d16542a1e6edfbd5c60
Zelalem <zelalemtenaw26@gmail.com>
2018-10-21 00:38:45 -0400

    Linked files together

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+# [start import]
+import os
+import urllib
+
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [end import]    
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = JINJA_ENVIRONMENT.get_template('Start_Page.html')
+        self.response.write(template.render(template_values))
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
